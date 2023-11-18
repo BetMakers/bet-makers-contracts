17f8c11dc2ffae6c5d87661c32fff124548a8e81
Daniel Beltran <danielbeltran@Daniels-MacBook-Pro.local>
2023-11-18 20:53:32 +0300

    vote token to deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -16,6 +16,10 @@ contract DeployBase is Script {
     );
 
     betMakers.setBetMaker(address(bMO));
+    ERC20Detailed voteToken= new ERC20Detailed();
+    voteToken.initialize("City", "CTY", 0);
+    voteToken.initialize("Lib", "LIB", 0);
+
 
     vm.stopBroadcast();
   }
