aa623e90e3f751d4ec3fd2817e4b039d7c8ad10a
Daniel Beltran <danielbeltran@Daniels-MacBook-Pro.local>
2023-11-18 21:15:59 +0300

    gfsfsd

--- src/tokenDeployer.sol
+++ src/tokenDeployer.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@solmate/src/tokens/ERC20.sol";
 
-contract tokenDeployer is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner, string memory name,string memory symbol ) Ownable(0xC2628eDdDB676c4cAF68aAD55d2191F6c9668624) ERC20(name, symbol) {
-        _mint(initialOwner, 10000);
-        transferOwnership(initialOwner);
+contract tokenDeployer is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol, 0) {
+        _mint(0xC2628eDdDB676c4cAF68aAD55d2191F6c9668624, 10000);
     }
 }
