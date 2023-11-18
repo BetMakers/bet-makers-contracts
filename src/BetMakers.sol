1605bfc964969dbda9333161fd64555dffe08f09
Daniel Beltran <danielbeltran@Daniels-MacBook-Pro.local>
2023-11-18 17:26:10 +0300

    added chainlink functions to get fotball match result

--- src/BetMakers.sol
+++ src/BetMakers.sol
@@ -3,10 +3,12 @@ pragma solidity =0.8.17;
 
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
 import {SafeTransferLib} from "@solmate/src/utils/SafeTransferLib.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 
-contract BetMakers {
+contract BetMakers is ChainlinkClient, ConfirmedOwner {
     using SafeTransferLib for ERC20;
-
+    using Chainlink for Chainlink.Request;
     address USDT = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F; 
 
     address betMakerAddress;
@@ -14,8 +16,25 @@ contract BetMakers {
     mapping(uint256 => mapping(uint256 => address[])) public poolParticipants; // uint pubId to uint256 result to betterAddress
     mapping(uint256 => uint256) public matchPool; // uint pubId to uint total pool // money
     mapping(uint256 => uint256) public matchBet; // uint pubId to uint bet ticket  // money
-  
-    constructor(){
+
+    // Chainlink Functions variables    
+    uint256 public volume;
+    bytes32 private jobId;
+    uint256 private fee;
+    /*
+     * @notice Initialize the link token and target oracle
+     *
+     * Mumbai Testnet details:
+     * Link Token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Oracle: 0x40193c8518BB267228Fc409a613bDbD8eC5a97b3 (Chainlink DevRel)
+     * jobId: ca98366cc7314957b8c012c72f05aeeb // to get an uint256
+     *
+     */
+    constructor() ConfirmedOwner(msg.sender){
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        setChainlinkOracle(0x40193c8518BB267228Fc409a613bDbD8eC5a97b3);
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
         owner = msg.sender;
     } 
 
@@ -59,10 +78,23 @@ contract BetMakers {
         } 
     }
 
-    function executeBet(uint256 pubId, uint256 result) external onlyOwner{
+    function executeBet(uint256 pubId) external onlyOwner{
       // chainlink api oracle to get actual result
-      uint256 winners = poolParticipants[pubId][result].length;
-      uint256 prize = matchPool[pubId]/winners;
+      requestVolumeData(matchId[pubId]);
+    }
+
+        /**
+     * Receive the response in the form of uint256
+     */
+    function fulfill(
+        bytes32 _requestId,
+        uint256 _volume
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestVolume(_requestId, _volume);
+        result = _volume;
+
+        uint256 winners = poolParticipants[pubId][result].length;
+        uint256 prize = matchPool[pubId]/winners;
       for (uint256 i; i < winners; ++i) {
           ERC20(USDT).safeTransfer(poolParticipants[pubId][result][i], prize);
           matchPool[pubId] -= prize;
@@ -71,6 +103,28 @@ contract BetMakers {
 
     function setBetMaker(address _betMakerAddress) external onlyOwner{
         betMakerAddress = _betMakerAddress;
+    }
+
+    function requestVolumeData(uint256 matchId) public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add(
+            "get",
+            "https://myfotballgameresults.io/${matchId}"
+        );
+
+        req.add("path", "winner"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10 ** 18;
+        req.addInt("times", timesAmount);
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
     }
 }
 
